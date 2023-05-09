7bae8395ad33f495092a11ee4edc770a0266bcba
Luc <lucasm@bempromotora.com.br>
2023-05-09 18:09:01 -0300

    Aula 3 - Tipos de dados e conversão de tipos

---
+++ Alura/C# Parte 1/Aula 3/0308_ConversaoTiposNumericos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _0308_ConversaoTiposNumericos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salario = 1100.50;
+        Console.WriteLine(salario);
+
+        int salarioEmInt = (int)salario;
+        Console.WriteLine(salarioEmInt);
+
+        float altura = 1.85f;
+        Console.WriteLine(altura);
+    // Float não pode ser convertido implicitamente em Double:
+        altura = (float)1.85;
+        Console.WriteLine(altura);
+    // Double pode ser convertido implicitamente em Float, mas fica esquisito:
+        double alturaDouble = 1.85f;
+        Console.WriteLine(alturaDouble);
+    }
+}
