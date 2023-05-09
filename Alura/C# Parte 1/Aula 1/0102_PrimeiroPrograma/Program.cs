e4be2b681bd8bfeb8fbd0a0b50f20d436ba72e50
Luc <lucasm@bempromotora.com.br>
2023-05-09 16:42:15 -0300

    Aula 1 - Primeiro programa e declaração/atribuição de variáveis.

---
+++ Alura/C# Parte 1/Aula 1/0102_PrimeiroPrograma/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _0102_PrimeiroPrograma;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string message = "Welcome to the NHK!";
+
+        Console.WriteLine(message);    }
+}
---
+++ Alura/C# Parte 1/Aula 1/0103_CriandoVariaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _0103_CriandoVariaveis;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 29;
+
+        Console.WriteLine("Minha idade hoje é " + idade);
+
+        idade += 1;
+
+        Console.WriteLine("Ano que vem, minha idade será " + idade);
+    }
+}
