a91fb573bffa215103b89b29fb164e7b34b98b86
Luc <lucasm@bempromotora.com.br>
2023-05-10 09:48:30 -0300

    Aula 4 - Variáveis de texto e variáveis guardando valores

---
+++ Alura/CSharpParte1/Aula04/0401_CaracteresTextos/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _0401_CaracteresTextos;
+class Program
+{
+    static void Main(string[] args)
+    {
+    // Caractere
+        char primeiraLetra = 'a';
+        Console.WriteLine(primeiraLetra);
+        primeiraLetra = (char)42;
+        Console.WriteLine(primeiraLetra);
+        primeiraLetra = (char)(42+7);
+        Console.WriteLine(primeiraLetra);
+        
+    // String
+        string titulo = "Eu tô com sono aaaaaaaaaaa " + 424242;
+        Console.WriteLine(titulo);
+
+        string cursosProgramacao = " - .NET" + 
+            " - Java" + 
+            " - JavaScript";
+        Console.WriteLine(cursosProgramacao);
+        cursosProgramacao = @" - .NET
+ - Java
+ - JavaScript";
+         Console.WriteLine(cursosProgramacao);
+    }
+}
---
+++ Alura/CSharpParte1/Aula04/0406_VariaveisValores/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _0406_VariaveisValores;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 29;
+        int idadeLuc = idade;
+        idade = 20;
+        Console.WriteLine(idade);
+        Console.WriteLine(idadeLuc);
+    }
+}
