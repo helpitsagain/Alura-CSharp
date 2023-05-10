d3998560d8414b4aeebafc8f0889f3c505a695e8
Luc <lucasm@bempromotora.com.br>
2023-05-10 16:25:15 -0300

    Aula 5 - Condicionais e booleanos

---
+++ Alura/CSharpParte1/Aula05/0502_Condicionais/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _0502_Condicionais;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idadeLuc = 29;
+
+        if (idadeLuc >= 18) {
+            Console.WriteLine("Maior de idade");
+        } else {
+            Console.WriteLine("Menor de idade");
+        }
+    }
+}
---
+++ Alura/CSharpParte1/Aula05/0504_Booleanos/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _0504_Booleanos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 14;
+        int quantidadePessoas = 2;
+        bool maiorIdade = idade >= 18;
+        bool acompanhado = quantidadePessoas >= 2;
+
+        if (maiorIdade || acompanhado) {
+            Console.WriteLine("Maior de idade ou menor acompanhado, entrada permitida.");
+        } else {
+            Console.WriteLine("Menor de idade, entrada negada.");
+        }
+    }
+}
