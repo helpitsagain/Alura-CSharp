ae51fc628028beafaa90bafb901c9e39cee180b7
Luc <lucasm@bempromotora.com.br>
2023-05-15 10:20:01 -0300

    Curso 02 Aula 06: Construtores e Membros Estáticos

---
+++ CSharpParte2/07-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07_ByteBank;
+class Program
+{
+    static void Main(string[] args)
+    {
+		ContaCorrente c = new ContaCorrente(42, 1337, "12345678910");
+		Console.WriteLine(ContaCorrente.QuantidadeContasCriadas);
+		
+		ContaCorrente c2 = new ContaCorrente(42, 1337, "12345678910");
+		Console.WriteLine(ContaCorrente.QuantidadeContasCriadas);
+
+		ContaCorrente c3 = new ContaCorrente(42, 1337, "12345678910");
+		Console.WriteLine(ContaCorrente.QuantidadeContasCriadas);
+
+    }
+}
