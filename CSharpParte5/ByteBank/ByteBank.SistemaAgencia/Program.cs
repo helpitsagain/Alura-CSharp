be0d067027de613c4dfdc44c6007a95312fe0c24
Luc (Bem) <lucasm@bempromotora.com.br>
2023-05-22 12:17:01 -0300

    Parte 05 Aula 05: NuGet

--- CSharpParte5/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ CSharpParte5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,17 +1,15 @@
 ﻿using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
 using ByteBank.Modelos.Sistema;
+using Humanizer;
 
-ContaCorrente c = new ContaCorrente(42, 1337);
-Console.WriteLine(c.Agencia);
+DateTime dataPagamento = new DateTime(2023, 12, 31);
+Console.WriteLine(dataPagamento);
 
-Diretor d = new Diretor("01351848003");
-d.Nome = "Diretoooor";
-Console.WriteLine(d.Nome);
+DateTime dataCorrente = DateTime.Now;
+Console.WriteLine(dataCorrente);
 
-ParceiroComercial p = new ParceiroComercial();
-p.Senha = "123";
-Console.WriteLine(p.Autenticar("123"));
-
-// AutenticacaoHelper a = new AutenticacaoHelper();
-// Console.WriteLine(a.CompararSenhas("123", "123"));
+TimeSpan diferenca = dataPagamento - dataCorrente;
+diferenca = TimeSpan.FromMinutes(45);
+string mensagem = $"Vencimento em {TimeSpanHumanizeExtensions.Humanize(diferenca)}";
+Console.WriteLine(mensagem);
